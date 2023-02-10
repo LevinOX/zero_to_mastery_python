2581bd761826923c6b07c383cfc0b5ba9bd93153
LevinOX <daniel.wrede@posteo.de>
2023-02-10 12:49:51 +0100

    finish selenium exercise

--- selenium/automation.py
+++ selenium/automation.py
@@ -4,11 +4,23 @@ from selenium.webdriver.common.by import By
 from time import sleep
 service = Service(executable_path='./chromedriver')
 chrome_browser = webdriver.Chrome(service=service)
-chrome_browser.maximize_window()
+# chrome_browser.maximize_window()
 chrome_browser.get('https://demo.seleniumeasy.com/basic-first-form-demo.html')
-button = chrome_browser.find_element(By.CLASS_NAME, 'btn-default')
-print(button.get_attribute('innerHTML'))
-# sleep(10)
+show_message_button = chrome_browser.find_element(By.CLASS_NAME, 'btn-default')
+print(show_message_button.get_attribute('innerHTML'))
+assert 'Show Message' in chrome_browser.page_source
+
+user_message = chrome_browser.find_element(By.ID, 'user-message')
+user_message.clear()
+user_message.send_keys('I am testing selenium.')
+
+show_message_button.click()
+output_message = chrome_browser.find_element(By.ID, 'display')
+assert 'I am testing selenium.' in output_message.text
+print(chrome_browser.find_element(By.CSS_SELECTOR, '#get-input > .btn'))
+
+sleep(1)
+chrome_browser.close()
 
 # if 'Selenium Easy Demo' in chrome_browser.title
 #   print("Yes in title.")
